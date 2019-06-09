664a71748656346f9adb767d670e8c0f01db8d3b
yututi <y.tsuchiya0207@gmail.com>
2019-06-09 22:44:04 +0900

    layout

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,25 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <layout>
+      <template v-slot:content>
+        <router-view/>
+      </template>
+    </layout>
   </div>
 </template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Layout from "@/views/Layout.vue"; // @ is an alias to /src
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+@Component({
+  components: {
+    Layout
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="stylus">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
---
+++ front/src/views/Layout.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app" @click="hideNav()">
+    <div class="app__header header">
+      <a class="header__left-menu" href="#" @click.prevent.stop="toggleNav">
+        <i class="fas fa-hamburger"></i>
+      </a>
+      <span>{{appname}}</span>
+      <div class="header__right-menu"></div>
+    </div>
+    <nav click.prevent.stop class="app__sidenav" :class="navCls">aa</nav>
+    <div class="app__body">
+      <slot name="content"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Layout extends Vue {
+  appname: string = "sample";
+  showNav: boolean = true;
+  showNavSp: boolean = false;
+
+  get navCls() {
+    return {
+      "app__sidenav--show-pc": this.showNav,
+      "app__sidenav--show-sp": this.showNavSp
+    };
+  }
+
+  toggleNav() {
+    this.showNav = !this.showNav;
+    this.showNavSp = true;
+  }
+  hideNav() {
+    this.showNavSp = false;
+  }
+
+  mounted() {}
+}
+</script>
+<style lang="stylus">
+@require '../styles/index.styl';
+</style>
